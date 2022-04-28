ce65a94427ab9922260b5ebbf401646bd6794e16
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-28 06:47:42 -0300

    using EntityManagerInterface
    using EntityManagerInterface to find all movies.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,24 +2,25 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'movies') ]
     public function index(): Response
     {
-        $movies = [
-            'The Shawshank Redemption',
-            'The Godfather',
-            'The Godfather: Part II',
-            'The Dark Knight',
-            '12 Angry' ];
-
-        return $this->render('index.html.twig', array(
-            'movies' => $movies
-        ));       
+        $repository = $this->em->getRepository('App:Movie');
+        $movies = $repository->findAll();
+        dd($movies);
+        return $this->render('index.html.twig');       
     }
 }
