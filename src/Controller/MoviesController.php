761c768af9ce3f470aef5266188308a4d537866c
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-30 23:28:01 -0300

    Update MoviesController.php
    +delete

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -104,6 +104,15 @@ class MoviesController extends AbstractController
         ]);
     }
 
+    #[Route('/movies/delete/{id}', methods: ['GET', 'DELETE'], name: 'delete_movie')]
+    public function delete($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        $this->em->remove($movie);
+        $this->em->flush();
+        return $this->redirectToRoute('movies');
+    }
+
     #[Route('/movies/{id}', methods: ['GET'], name: 'show_movie') ]
     public function show($id): Response
     {
